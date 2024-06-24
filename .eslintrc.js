34904df1d6ecd13e15331cabedfff249fb02292c
youpiteron <oyushchenko@kse.org.ua>
2024-06-24 11:19:11 +0300

    Initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    env: {
+      browser: true,
+      es2021: true,
+      'jest/globals': true
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended',
+      'plugin:storybook/recommended'
+    ],
+    plugins: ['react', 'react-hooks', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react/no-unescaped-entities': 0
+    },
+    globals: {
+      process: true
+    }
+  }
