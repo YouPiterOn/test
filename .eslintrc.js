c0e459fdcd611676bc89d412e4bb72c5d4b159e0
youpiteron <oyushchenko@kse.org.ua>
2024-06-30 16:41:39 +0300

    workflow test commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
     parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true
-      }
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
     },
     env: {
-      browser: true,
-      es2021: true,
-      'jest/globals': true
+        browser: true,
+        es2021: true,
+        'jest/globals': true
     },
     extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:prettier/recommended',
-      'plugin:storybook/recommended'
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'plugin:storybook/recommended'
     ],
     plugins: ['react', 'react-hooks', 'prettier'],
     rules: {
@@ -24,6 +24,6 @@ module.exports = {
         'react/no-unescaped-entities': 0
     },
     globals: {
-      process: true
+        process: true
     }
-  }
+}
